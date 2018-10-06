9fae8653cfeb9c6482345f41b325564fa7109fb0
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-06 22:13:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+
+    class Demo
+    {
+        int mdata;
+        public Demo()
+        {
+            mdata = 45;
+        }
+        public Demo(int mdata)
+        {
+
+            this.mdata = mdata;
+        }
+        public void show()
+        {
+            Console.WriteLine(mdata);
+        }
+    }
+    class Test
+    {
+        static void Main()
+        {
+            Demo x = new Demo();
+            x.show();
+            Demo y = new Demo(44);
+            y.show();
+            Console.ReadLine();
+        }
+
+    }
+}
